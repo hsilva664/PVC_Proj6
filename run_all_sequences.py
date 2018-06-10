d6245c0a9eb15c38d8e2335cb0f4420d3d54b1d8
hsilva664 <metallic-silver@hotmail.com>
2018-06-10 06:22:29 -0300

    Adding files

---
+++ run_all_sequences.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import draw_tracking_sequence
+
+basedir_list = ['PD8-files/car1','PD8-files/car2']
+txt_filename_list = ['PD8-files/gtcar1.txt','PD8-files/gtcar2.txt']
+
+
+tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW']
+# GOTURN does not work (bugged implementation)
+
+draw = "False"
+
+for i in range(len(basedir_list)):
+    basedir = basedir_list[i]
+    txt_filename = txt_filename_list[i]
+
+    for j in range(len(tracker_types)):
+        tracker_type = tracker_types[j]
+        out_filename = 'results/' + tracker_type + '_' + str(i) + '.txt'
+
+        in_args = [None, basedir, txt_filename, tracker_type, out_filename, draw]
+
+        draw_tracking_sequence.run(in_args)
